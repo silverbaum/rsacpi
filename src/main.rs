ce3204103aa65652b96a2011b86e185644ddda1b
silverbaum <topias.silfverhuth@gmail.com>
2025-04-11 16:42:38 +0300

    idiomatic?

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::{fs::{self, read_dir}, io, env};
 
 
-fn bat(dir: &str) -> io::Result<()> 
+fn bat(dir: &str) -> Result<String, io::Error>
 {
     for entry_r in read_dir(dir)? {
         let entry = entry_r?;
         let file_namebuf = entry.file_name();
-        let file_name = file_namebuf.to_str().unwrap();
+        let file_name = file_namebuf.to_str().unwrap_or("Error in to_str");
 
         if file_name.contains("BAT") || file_name.contains("bat") && entry.file_type()?.is_dir() 
         {
@@ -15,88 +15,143 @@ fn bat(dir: &str) -> io::Result<()>
                 //let mut capbuf: [u8; 1] = [0; 1];
                 //let _ = capacity.read(&mut capbuf);
                 let cap: i32 = capacity.trim().parse().unwrap();
-                
-                let status = fs::read_to_string(format!("{}/status", entry.path().to_string_lossy()))?;
-                print!("{file_name}: {}%, {}", cap, status);
+                let status = match fs::read_to_string(format!("{}/status", entry.path().to_string_lossy())) {
+                Ok(str) => str, Err(_) => "Unknown".to_owned()};
+
+                return Ok(String::from( format!("{file_name}: {}%, {}", cap, status) ));
 
         }
     }
 
-    Ok(())
+    Err(io::Error::other("No batteries found"))
 }
 
 
-fn thermal(thermdir: &str) -> io::Result<()>
+fn thermal(thermdir: &str) -> Result<(), io::Error>
 {
+    let mut found: bool = false;
     let mut entries = read_dir(thermdir)?
                     .map(|i| i.map(|e| e.path().to_owned()))
                     .collect::<Result<Vec<_>, io::Error>>()?;
 
     entries.sort();
-    
-    for f in entries {
-        let file_path = f.to_str().unwrap();
-        
-        if file_path.contains("thermal_zone"){
-                let file_name = file_path.strip_prefix(format!("{thermdir}/thermal_zone").as_str()).unwrap();
-                let mut temp = fs::read_to_string(format!("{}/temp", file_path))?;
-                let tempi: i32 = match temp.trim().parse() {
-                    Ok(n) => n,
-                    Err(_) => 0
-                };
-                temp = (tempi / 1000).to_string();
 
-                println!("Thermal {}: {}C", file_name, temp);
+   // let mut batteries = Vec::<String>::new;
+    
+    for f in entries.iter() {
+        let file_path = f.to_str().unwrap_or("err: to_str");
+        
+        if file_path.contains("thermal_zone") {
+                found = true;
+                let file_name: &str = file_path.strip_prefix(format!("{thermdir}/thermal_zone").as_str()).unwrap();
+                let temp: String = fs::read_to_string(format!("{}/temp", file_path))?;
+                let tempi: f32 = match temp.trim().parse::<f32>() {
+                    Ok(n) => n/1000.0,
+                    Err(_) => 0.0
+                };
+
+                println!("Thermal {}: {:.1} C°", file_name, tempi);
         }
     }
 
-    Ok(())
+    match found {
+    true => Ok(()),
+    false => Err(io::Error::other("No thermals found"))
+    }
 }
 
-fn ac(acdir: &str) -> io::Result<()> {
-      for entry_r in read_dir(acdir)? {
-        let entry = entry_r?;
-        let file_namebuf = entry.file_name();
-        let file_name = file_namebuf.to_str().unwrap_or_default();
-        
-        if file_name.contains("AC") || file_name.contains("ac") && entry.file_type()?.is_dir() 
-        {
+
+fn ac(acdir: &str) -> Result<String, io::Error> 
+{
+    let no_ac: Result<String, io::Error> = Err(io::Error::other("No AC adapter found"));
+
+    for entry_r in read_dir(acdir)?     {
+
+      match entry_r {
+        Ok(ent) => {
+            let entry = ent; 
+            let file_namebuf = entry.file_name();
+            let file_name = file_namebuf.to_str().unwrap_or("Error");
+
+            if file_name.contains("AC") || file_name.contains("ac") && entry.file_type()?.is_dir() 
+            {
                 let ac_online = fs::read_to_string(format!("{}/online", entry.path().to_str().unwrap()))?;
                 let ac_status: i32 = ac_online.trim().parse().unwrap();
 
                 let ac_str: &str;
-                if ac_status == 1 {
-                    ac_str = "online";
-                } else {
-                    ac_str = "off-line";
+                match ac_status {
+                    1 => ac_str = "on-line",
+                    0 => ac_str = "off-line",
+                    _ => ac_str = "Unknown",
                 }
 
-                print!("{file_name}: {}\n", ac_str);
-        }
-    }
+                return Ok(String::from(format!("{file_name}: {}", ac_str)));
+            }
+            }
+    
+        Err(_) => return no_ac
+      }
+      }
 
-    Ok(())
+      no_ac
 }
 
-fn main() {
+fn help() 
+{
+println!("rsacpi [OPTION]\n\n\
+-b  Display Battery information\n\
+-a  Display AC adapter information\n\
+-t  Display thermal zone temperatures\n\
+-A  Prints all available information\n\
+-h  Display this help information and exit")
+}
+
+
+fn main()
+{
     let batdir = "/sys/class/power_supply";
     
     let args: Vec<String> = env::args().collect();
     if args.len() == 1 {
-        let _ = bat(batdir);
+        match bat(batdir) {
+        Ok(str) => print!("{str}"),
+        Err(e) => println!("{e}")
+        }
         return;
     }
 
     if args[1].contains("b") {
-        let _ = bat(batdir);
+        
+        match bat(batdir) {
+        Ok(str) => print!("{str}"),
+        Err(e) => println!("{e}")
+        }
+
     } else if args[1].contains("t") {
-        let _ = thermal("/sys/class/thermal");
+        thermal("/sys/class/thermal").unwrap_or_else(|e| println!("{e}"));
     } else if args[1].contains("a") {
-        let _ = ac("/sys/class/power_supply");
+        
+        match ac("/sys/class/power_supply") {
+        Ok(str) => println!("{str}"),
+        Err(e) => println!("{e}")
+        
+        }
     } else if args[1].contains("A") {
-        let _ = bat(batdir);
-        let _ = ac(batdir);
-        let _ = thermal("/sys/class/thermal");
+        
+        match bat(batdir) {
+        Ok(str) => print!("{str}"),
+        Err(e) => println!("{e}")
+        }
+        
+        match ac(batdir) {
+        Ok(str) => println!("{str}"),
+        Err(e) => println!("{e}"),
+        }
+
+        thermal("/sys/class/thermal").unwrap_or_else(|e| println!("{e}"));
+
+    } else if args[1].contains("h") {
+        let _ = help();
     }
 
     
