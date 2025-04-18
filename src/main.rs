17dce9ccf1b97ea488a2eae1906c7fdf2a272e86
silverbaum <topias.silfverhuth@gmail.com>
2025-04-18 23:30:34 +0300

    added battery health option

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
 //Copyright (C) 2025 Topias Silfverhuth
 //SPDX-License-Identifier: MIT
 
-use std::{fs::{self, read_dir}, io};
+use std::{
+    fs::{read_dir, read_to_string, read}, 
+    process::exit,
+    io
+};
 
-const VERSION: &str = "0.3.0";
+const VERSION: &str = "0.4.0";
 
 struct Args {
-///Display battery information
+///battery information
 battery: bool,
-///Display AC adapter information
+///Battery health information
+battery_health: bool,
+/// AC adapter information
 ac: bool,
-///Display thermal information
+/// thermal information
 thermal: bool,
-///Display all the available information
+/// all the available information
 everything: bool,
 ///help me
 help: bool,
@@ -22,6 +28,48 @@ version: bool
 
 const PWDIR: &str = "/sys/class/power_supply";
 const THDIR: &str = "/sys/class/thermal";
+
+
+fn battery_design(dir: &str) -> Result<Vec<String>, io::Error> {
+    let mut batteries: Vec<String> = Vec::new();
+
+    let entries: Vec<_> = read_dir(dir)?
+                .map(|e| {e.unwrap_or_else(|er| panic!("{er}"))})
+                .filter(|entry| {
+                    entry.file_name().to_string_lossy().contains("BAT") 
+                 || entry.file_name().to_string_lossy().contains("bat")
+                })
+                .collect();
+    for entry in entries.iter() {
+
+        let fname = entry.file_name();
+        let file_name = fname.to_str().unwrap();
+
+        let design_capr = match read_to_string(format!("{}/energy_full_design", entry.path().to_string_lossy())) {
+            Ok(str) => str,
+            Err(_) => "Unknown".to_owned()
+            };
+        let design_cap: i32 = match design_capr.trim().parse::<i32>() {
+            Ok(int) => int / 10000,
+            Err(e) => return Err(io::Error::other(format!("Unable to determine battery's design capacity: {e}")))
+        };
+
+        let real_capr = match read_to_string(format!("{}/energy_full", entry.path().to_string_lossy())) {
+            Ok(str) => str,
+            Err(_) => "Unknown".to_owned()
+            };
+        let real_cap: i32 = match real_capr.trim().parse::<i32>() {
+            Ok(int) => int / 10000,
+            Err(e) => return Err(io::Error::other(format!("Unable to determine battery capacity: {e}")))
+        };
+        let battery_health : f64 = (<i32 as Into<f64>>::into(real_cap) / <i32 as Into<f64>>::into(design_cap)) * 100.0;
+        
+
+        batteries.push(format!("{file_name}: {real_cap} / {design_cap} mAh = {:.1}%", battery_health));
+    }
+
+    Ok(batteries)
+}
 
 ///Displays battery information
 fn bat(dir: &str) -> Result<Vec<String>, io::Error>
@@ -43,15 +91,13 @@ fn bat(dir: &str) -> Result<Vec<String>, io::Error>
         let file_name = file_namebuf.to_str().unwrap();
 
         let cap_path = format!("{}/capacity", entry.path().to_str().unwrap_or("/sys/class/power_supply/BAT0/capacity"));
-        let capacity_r = fs::read_to_string(&cap_path)?;
+        let capacity_r = read_to_string(&cap_path)?;
         let capacity: i32 = match capacity_r.trim().parse() {
             Ok(cap) => cap,
             Err(_) => -1
         };
-
         
-        
-        let status_r = match fs::read_to_string(format!("{}/status", entry.path().to_string_lossy())) {
+        let status_r = match read_to_string(format!("{}/status", entry.path().to_string_lossy())) {
                         Ok(str) => str,
                         Err(_) => "Unknown".to_owned()
                         };
@@ -60,11 +106,11 @@ fn bat(dir: &str) -> Result<Vec<String>, io::Error>
 
         //read raw charge and power consumption for time estimate
         let energy_path = format!("{}/energy_now", entry.path().to_str().expect("Failed to convert to string"));
-        let energy_r = fs::read_to_string(&energy_path)?;
+        let energy_r = read_to_string(&energy_path)?;
         let energy: i64 = match energy_r.trim().parse() {
             Ok(int) => int,
             Err(_) => {
-                let reader = fs::read(&energy_path)?;
+                let reader = read(&energy_path)?;
                 let res: i64 = unsafe {(*reader.align_to::<i64>().1)[0]};
                 if res >= 0 {
                     res
@@ -73,7 +119,7 @@ fn bat(dir: &str) -> Result<Vec<String>, io::Error>
                 }
             }};
             
-        let power_r = fs::read_to_string(format!("{}/power_now", entry.path().to_str().unwrap_or_else(|| "oh no")));
+        let power_r = read_to_string(format!("{}/power_now", entry.path().to_str().unwrap_or_else(|| "oh no")));
         let power: i64 = match power_r {
             Ok(power_r) => power_r.trim().parse().unwrap_or_else(|_| -1),
             Err(_) => -1
@@ -94,7 +140,7 @@ fn bat(dir: &str) -> Result<Vec<String>, io::Error>
             if capacity >= 0 {
                 batteries.push(String::from( format!("{file_name}: {}%, {}", capacity, status) ));
             } else {
-                let capbuf = fs::read(&cap_path)?;
+                let capbuf = read(&cap_path)?;
                 let capstr = String::from_utf8(capbuf).unwrap();
                 batteries.push(String::from( format!("{file_name}: {}%, {}", capstr.trim(), status) ));
 
@@ -110,7 +156,7 @@ fn bat(dir: &str) -> Result<Vec<String>, io::Error>
     }
 }
 
-///iterates through thermdir and prints the temp of all thermal zones
+///Thermal zones
 fn thermal(thermdir: &str) -> Result<Vec<String>, io::Error>
 {
     let mut found = false;
@@ -129,7 +175,7 @@ fn thermal(thermdir: &str) -> Result<Vec<String>, io::Error>
                     
     for f in files.iter() {
         let num = f.strip_prefix(format!("{thermdir}/thermal_zone").as_str()).unwrap();
-        let temp: String = fs::read_to_string(format!("{}/temp", f))?;
+        let temp: String = read_to_string(format!("{}/temp", f))?;
         let tempi: f32 = match temp.trim().parse::<f32>() {
         Ok(n) => n/1000.0,
         Err(_) => 0.0
@@ -162,7 +208,7 @@ fn ac(acdir: &str) -> Result<String, io::Error>
 
             if file_name.contains("AC") || file_name.contains("ac") && entry.file_type()?.is_dir() 
             {
-                let ac_online = fs::read_to_string(format!("{}/online", entry.path().to_str().unwrap()))?;
+                let ac_online = read_to_string(format!("{}/online", entry.path().to_str().unwrap()))?;
                 let ac_status: i32 = ac_online.trim().parse().unwrap();
 
                 let ac_str: &str;
@@ -182,16 +228,21 @@ fn ac(acdir: &str) -> Result<String, io::Error>
 
       no_ac
 }
-
-
 fn usage() {
+    println!("Usage: rsacpi [-bita] [-e] [-h] [-v]\n\
+see rsacpi --help for more help information");
+    exit(1);
+}
+
+fn help() {
 println!("Usage: rsacpi [OPTION]...\n\n\
--b, --battery       Displays battery information\n\
--t, --thermal       Displays temperatures from all thermal zones\n\
--a, --ac            Displays AC adapter status\n\
--e, --everything    Displays all available information\n\
--h, --help          Displays command-line options\n\
--v, --version       Displays version and license information");
+-b, --battery       battery information\n\
+-i, --health        battery health\n\
+-t, --thermal       temperatures from all thermal zones\n\
+-a, --ac            AC adapter status\n\
+-e, --everything    all available information\n\
+-h, --help          command-line options\n\
+-v, --version       version and license information");
 }
 
 fn version() {
@@ -200,7 +251,8 @@ a simple tool to display battery, AC, and thermal info\n\
 Copyright (C) 2025 Topias Silfverhuth\n\
 License: MIT <https://opensource.org/license/mit>\n\
 This is free software: you are free to change and redistribute it.\n\
-There is NO WARRANTY, to the extent permitted by law.");
+There is NO WARRANTY, to the extent permitted by law.\n\
+Report bugs at: <https://github.com/silverbaum/rsacpi>");
 }
 
 ///poor argument reaping and sowing embolism (parse)
@@ -208,6 +260,7 @@ fn parse() -> Args {
 
     let mut ac: bool = false;
     let mut battery: bool = false;
+    let mut battery_health: bool = false;
     let mut thermal: bool = false;
     let mut everything: bool = false;
     let mut help: bool = false;
@@ -227,74 +280,84 @@ fn parse() -> Args {
                 ac = true;
             } else if arg.contains("version") {
                 version = true;
+            } else if arg.contains("health") {
+                battery_health = true;
+            } else {
+                usage();
             }
         } else if arg.contains("-") && arg.starts_with("-") && !arg.starts_with("--") {
-            if arg.contains("a") {
-                ac = true;
-            } else if arg.contains("b") {
-                battery = true;
-            } else if arg.contains("t") {
-                thermal = true;
-            } else if arg.contains("e") {
-                everything = true;
-            } else if arg.contains("h") {
-                help = true;
-            } else if arg.contains("v") {
-                version = true;
-            } else {
-                println!("Unknown argument {arg}");
-                help = true;
+            let opts = arg.as_bytes();
+            for opt in opts {
+                match opt {
+                    b'a' => ac=true,
+                    b'b' => battery=true,
+                    b't' => thermal=true,
+                    b'e' => everything=true,
+                    b'h' => help=true,
+                    b'v' => version=true,
+                    b'i' => battery_health=true,
+                    b'-' => (),
+                    _ => usage()
+                }
             }
         }
             }
 
 
-    Args {battery, ac, thermal, everything, help, version}
+    Args {battery, battery_health, ac, thermal, everything, help, version}
 }
 
 
 fn main() -> io::Result<()>
 {
-       
     let mut args = parse();
 
     if args.help {
-        usage();
-        return Ok(());
+            help();
+            return Ok(());
     } else if args.version {
-        version();
-        return Ok(());
+            version();
+            return Ok(());
     }
     
     if args.everything {
-        args.battery = true; args.ac = true; args.thermal = true;
+            args.battery = true; args.battery_health = true; args.ac = true; args.thermal = true;
     }
 
     if args.battery ||
-    (!args.battery && !args.ac && !args.everything && !args.thermal){
-        match bat(PWDIR) {
-        Ok(str) => {
-            for bat in str {
-                println!("{bat}");
-            }},
-        Err(e) => println!("{e}")
-        }
+    (!args.battery && !args.ac && !args.everything && !args.thermal && !args.battery_health){
+            match bat(PWDIR) {
+            Ok(str) => {
+                for bat in str {
+                        println!("{bat}");
+                }},
+            Err(e) => println!("{e}")
+            }
     }
+    if args.battery_health {
+            match battery_design(PWDIR) {
+                Ok(vec) => for bat in vec {
+                    println!("{bat}");
+                },
+                Err(_) => ()
+            }
+    }
+
     if args.ac {
-        match ac(PWDIR) {
-        Ok(str) => println!("{str}"),
-        Err(e) => println!("{e}")
+            match ac(PWDIR) {
+            Ok(str) => println!("{str}"),
+            Err(e) => println!("{e}")
         }
     }
     if args.thermal {
-        match thermal(THDIR) {
-        Ok(temps) => {
-            for temp in temps {
-                println!("{temp}");
+            match thermal(THDIR) {
+            Ok(temps) => {
+                for temp in temps {
+                    println!("{temp}");
+                }
+            },
+            Err(e) => return Err(io::Error::other(e))
             }
-        },
-        Err(e) => return Err(io::Error::other(e))
-        }
     }
     
     Ok(())
@@ -379,6 +442,21 @@ use super::*;
     fn tst_main() {
         let result = main();
         assert!(result.is_ok())
+    }
+
+    #[test]
+    fn tst_health() {
+        let result = battery_design(PWDIR);
+        
+        let e = match result {
+            Ok(vec) => {if vec[0].contains("mAh") && vec[0].contains("%") {
+                true
+            } else {
+                false
+            }},
+            Err(_) => false
+        };
+        assert!(e)
     }
 
 
